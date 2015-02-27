51f32f96244118a00b9cad8d01633490e81ac7b4
peader <kiltale@gmail.com>
2015-02-27 15:07:01 +0200

    created the config file for euro exchange rates
    Added the time stamp for the current exchange rate

--- Currency Scraper.py
+++ Currency Scraper.py
@@ -3,10 +3,16 @@ url = 'www.themoneyconverter.com/rss-feed/EUR/rss.xml'
 websitepath = 'http://' + url
 website = urllib.urlopen(websitepath)
 utext = website.readlines()
+EuroExchangeconfigfile = open('C:/Users/peter/Documents/GitHub/TravelBudget/config.txt','w')
 for line in utext:
-    print line
+    if '<lastBuildDate>' in line:
+      string = ('LAST UPDATED: ' + line[line.index('<lastBuildDate>') + 15:line.rindex('</lastBuildDate>')])
+      EuroExchangeconfigfile.write(string)
+      #print line.index('<lastBuildDate>') + 15
+    #print line
 #print utext
 #websiteholder = open('C:/Users/peter/Documents/600.1xFiles/Messing/Urltext.txt','w')
 #l = utext.find('Login')
 #websiteholder.write(utext[l:l+6])
-#website.close()
+website.close()
+EuroExchangeconfigfile.close()
