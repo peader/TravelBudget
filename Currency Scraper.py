deec15e24025e79ecf407e118cb56e91f0813d1a
peader <kiltale@gmail.com>
2015-02-27 14:41:03 +0200

    initial commit
    python file for getting current currency exchanges

--- Currency Scraper.py
+++ Currency Scraper.py
@@ -1,1 +1,12 @@
-
+import urllib 
+url = 'www.themoneyconverter.com/rss-feed/EUR/rss.xml'
+websitepath = 'http://' + url
+website = urllib.urlopen(websitepath)
+utext = website.readlines()
+for line in utext:
+    print line
+#print utext
+#websiteholder = open('C:/Users/peter/Documents/600.1xFiles/Messing/Urltext.txt','w')
+#l = utext.find('Login')
+#websiteholder.write(utext[l:l+6])
+#website.close()
