294bf3717486e7a0210c325cc85582accc3c2c30
peader <kiltale@gmail.com>
2015-02-28 22:35:07 +0200

    Some clean up

--- Main.py
+++ Main.py
@@ -43,14 +43,15 @@ class Currency(object):
       if  isinstance(self,EuroExchangeRates):
         text = 'EUR'  
         url = 'www.themoneyconverter.com/rss-feed/' + text + '/rss.xml'
+        configfilename = 'C:/Users/peter/Documents/GitHub/TravelBudget/' + text + 'config.txt'
         
         websitepath = 'http://' + url
-        try:
-          website = urllib.urlopen(websitepath)
-          EuroExchangeconfigfile = open('C:/Users/peter/Documents/GitHub/TravelBudget/' + text + 'config.txt','w')
-        except:
-            print 'Cannot access online exchange rates. Using default values'
-            return
+      try:
+        website = urllib.urlopen(websitepath)
+        Exchangeconfigfile = open(configfilename,'w')
+      except:
+        print 'Cannot access online exchange rates. Using default values'
+        return
         
       utext = website.readlines()
       
@@ -59,29 +60,29 @@ class Currency(object):
       for line in utext:
         if '<lastBuildDate>' in line:
           string = ('LAST UPDATED: ' + line[line.index('<lastBuildDate>') + 15:line.rindex('</lastBuildDate>')] + '\n')
-          EuroExchangeconfigfile.write(string)
+          Exchangeconfigfile.write(string)
       # Skip the first two '<title>' headings
         if '<title>' in line:
           if  firstitle < 2:
               firstitle += 1
           else:
             string = ('TITLE: ' + line[line.index('<title>') + 7:line.rindex('</title>')]  + '\n' )
-            EuroExchangeconfigfile.write(string)
+            Exchangeconfigfile.write(string)
         #Skip the first <description>
         if '<description>' in line:
           if not firstdescrip:
               firstdescrip = True
           else:
             string = ('DESCRIPTION: ' +line[line.index('<description>') + 13:line.rindex('</description>')]  + '\n')
-            EuroExchangeconfigfile.write(string)
+            Exchangeconfigfile.write(string)
         if '<category>' in line:
           string = ('CATEGORY: ' +line[line.index('<category>') + 10:line.rindex('</category>')] + '\n' + '\n' )
-          EuroExchangeconfigfile.write(string)
+          Exchangeconfigfile.write(string)
     
     
 
       website.close()
-      EuroExchangeconfigfile.close()
+      Exchangeconfigfile.close()
       
     def getDateUpdated(self):
         return self.LastUpdated
