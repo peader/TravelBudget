d10c7e4fc0780ce08a88bc08b17a5096fbc40c40
peader <kiltale@gmail.com>
2015-02-28 21:53:11 +0200

    Implemented the exchange currency class

--- Main.py
+++ Main.py
@@ -1,26 +1,57 @@
+#import string
+
 #Set up classes
-class Money(Object):
-    def __init__(self):
-        pass
-    
-class Euro(Money):
-  def __init__(self):
-      pass
-
-  def ReadEuroConfigFile():
-   EuroExchangeconfigfile = open('C:/Users/peter/Documents/GitHub/TravelBudget/config.txt','r')
-   lines =  EuroExchangeconfigfile.readlines()
-
+class Currency(object):
+    def __init__(self, configFilename ):
+        """ configFile = string containing the full path to the config file"""
+        self.configFilename = configFilename
+        self.ExchangeDict = {}        
+        self.LastUpdated = self.ReadConfigFile(configFilename)
+        
+        
+     # Read config file
+    def ReadConfigFile(self,filename):
+      EuroExchangeconfigfile = open(filename,'r')
+      lines =  EuroExchangeconfigfile.readlines()
+      #print lines
+      ExchangeDictVars = []
+      DateUpdated = ''
+      key = ''
    
-   for line in lines:
+      for line in lines:
+        #print line
+        if 'LAST UPDATED:' in line:
+           DateUpdated = line[line.index('LAST UPDATED:') + 14:]
+           DateUpdated = DateUpdated.replace('\n','')
+        elif 'TITLE:' in line:
+           key = line[line.index('TITLE:') + 7:]
+           key = key.replace('\n','')
+        elif 'DESCRIPTION: 1 Euro =' in line:
+           ExchangeDictVars = line[line.index('DESCRIPTION: 1 Euro =') + 21:].split(' ')
+           ExchangeDictVars[2] = ' '.join(ExchangeDictVars[2:])
+           ExchangeDictVars[2] = ExchangeDictVars[2].replace('\n','')
+           #print ExchangeDictVars[1:]
+           self.ExchangeDict[key] = ExchangeDictVars[1:3]
+         
+      EuroExchangeconfigfile.close() 
+         
+      return DateUpdated
+      
+    def getDateUpdated(self):
+        return self.LastUpdated
+        
+    def getExchangeDict(self):
+        return self.ExchangeDict
+      
+      
        
-    
-
-EuroExchangeconfigfile.close()      
-    
         
         
-
     
-
+class EuroExchangeRates(Currency):
+      pass
+      
+      
+      
+    
 
